55b0810d7ebd9c3c2a627a9547ec97ef78676b8a
Fukyrdrs <artm.filipppov@mail.ru>
2024-07-29 00:42:09 +0300

    Lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -2,7 +2,7 @@ name = 'Artem'
 print('Name:',name)
 age = 22
 print('Age:', age)
-new_age = 23
-print('New age:', new_age)
-is_student = 'true'
+new_age = 1
+print('New age:', age + new_age)
+is_student = True
 print('Is student:',is_student)
